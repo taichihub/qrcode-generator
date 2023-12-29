cd8725ccbc364619549e67b14a5268f51279e07f
taichihub <taichikuniv21@gmail.com>
2023-12-30 05:26:24 +0900

    オプション有でQRコードを生成できるようにした

--- src/generate_qr.py
+++ src/generate_qr.py
@@ -1,6 +1,6 @@
 import qrcode
 from PIL import Image
-from setting import QR_VERSION, ERROR_CORRECTION_LEVEL, BOX_SIZE, BORDER_SIZE
+from setting import QR_VERSION, ERROR_CORRECTION_LEVEL, BOX_SIZE, BORDER_SIZE, QR_COLOR, QR_BACKGROUND_COLOR, QR_BACKGROUND_TRANSPARENT
 
 def generate_qr_code(url, filename):
   qr = qrcode.QRCode(
@@ -11,5 +11,19 @@ def generate_qr_code(url, filename):
   )
   qr.add_data(url)
   qr.make(fit=True)
-  img = qr.make_image(fill_color="black", back_color="white")
+
+  if QR_BACKGROUND_TRANSPARENT:
+    img = qr.make_image(fill_color=QR_COLOR)
+    img = img.convert("RGBA")
+    datas = img.getdata()
+    newData = []
+    for item in datas:
+      if item[0] == 255 and item[1] == 255 and item[2] == 255:  # 白色の部分（背景）を透明に
+        newData.append((255, 255, 255, 0))
+      else:
+        newData.append(item)
+    img.putdata(newData)
+  else:
+    img = qr.make_image(fill_color=QR_COLOR, back_color=QR_BACKGROUND_COLOR)
+
   img.save(filename)
--- src/setting.py
+++ src/setting.py
@@ -5,3 +5,6 @@ QR_VERSION = 1 # QRコードのバージョン
 ERROR_CORRECTION_LEVEL = qrcode.constants.ERROR_CORRECT_L  # 7%のエラー訂正能力を持つレベル
 BOX_SIZE = 10 # ボックスサイズ
 BORDER_SIZE = 4 # 境界線のサイズ
+QR_COLOR = "#000000" # QRコードの色（デフォルトは黒色, 黒：#000000）
+QR_BACKGROUND_COLOR = "#FFFFFF" # QRコードの背景色（デフォルトは白色, 白：#FFFFFF）
+QR_BACKGROUND_TRANSPARENT = True  # True：背景透明化有効, False：背景透明化無効
